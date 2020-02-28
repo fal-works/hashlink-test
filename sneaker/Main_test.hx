c2d93abe6621d7e3d97b3a4d68cb8693df3194fc
FAL <falworks.contact@gmail.com>
2020-02-29 04:30:22 +0900

    edit test

--- sneaker/Main_test.hx
+++ sneaker/Main_test.hx
@@ -3,75 +3,82 @@ import sneaker.unit_test.TesterSettings;
 import sneaker.assertion.Asserter.assert;
 import sneaker.print.Printer.println;
 
+class GroupA {
+  // Each test case should be wrapped with testCase(),
+  // specifying a type of expected result (Ok/Fail/Visual)
+  static final case1 = testCase(
+    () -> {
+      describe("This goes without error.");
+      assert(1 < 2);
+    },
+    Ok // Ok means this case passes if it completes without error
+  );
+
+  static final case2 = testCase(
+    () -> {
+      describe("This should go without error...");
+      final a = 9999;
+      final b = 1;
+      assert(a < b);
+    },
+    Ok // Although declared as Ok, this case fails (not expected)
+	);
+
+  // testCaseGroup() wraps multiple test cases
+  public static final all = testCaseGroup([
+    case1,
+    case2
+  ]);
+}
+
+class GroupB {
+  static final case1 = testCase(
+    () -> {
+      describe("This should fail.");
+      assert(9999 < 1);
+    },
+    Fail // Fail means this case passes if it raises an exception
+  );
+
+  static final case2 = testCase(
+    () -> {
+      describe("This should fail and raise an exception.");
+      final a = 1;
+      final b = 9999;
+      assert(a < b);
+    },
+    Fail // Although declared as Fail, this case succeeds (not expected)
+	);
+
+  public static final all = testCaseGroup([
+    case1,
+    case2
+  ]);
+}
+
+class GroupC {
+  static final case1 = testCase(
+    () -> {
+      describe("This prints AAA.");
+      println("AAA");
+    },
+    Visual // Visual means this case needs to be judged visually
+	);
+
+	public static final all = case1; // No difference whether grouped or not
+}
+
 class Main {
-	static var caseA1 = testCase(
-		() -> {
-			describe("This goes without error.");
-			final a = 1;
-			final b = 2;
-			assert(a < b);
-		},
-		Ok // "Ok" means this case passes if it completes without error
-	);
+  // Nested groups, if needed
+  static final allCases = testCaseGroup([
+    GroupA.all,
+    GroupB.all,
+    GroupC.all
+  ]);
 
-	static var caseA2 = testCase(
-		() -> {
-			describe("This goes without error.");
-			final a = 10000; // !?
-			final b = 2;
-			assert(a < b);
-		},
-		Ok // Although declared as "Ok", this case actually fails
-	);
+  static function main() {
+    TesterSettings.hidePassedResults = true; // Just as the name says
 
-	static var caseB1 = testCase(
-		() -> {
-			describe("This should fail.");
-			final a = 10000;
-			final b = 2;
-			assert(a < b);
-		},
-		Fail // "Fail" means this case passes if it raises an exception"
-	);
-
-	static var caseB2 = testCase(
-		() -> {
-			describe("This should fail and throw anything.");
-			final a = -10000; // !?
-			final b = 2;
-			assert(a < b);
-		},
-		Fail // Although declared as "Fail", this case actually succeeds
-	);
-
-	static var caseC1 = testCase(
-		() -> {
-			describe("This prints \"AAA\".");
-			println("AAA");
-		},
-		Visual // "Visual" means this case needs to be judged visually
-	);
-
-	// This groups multiples test cases
-	static var caseGroupA = testCaseGroup([
-		caseA1,
-		caseA2
-	]);
-	static var caseGroupB = testCaseGroup([
-		caseB1,
-		caseB2
-	]);
-
-	// Nested groups, if needed
-	static var allCases = testCaseGroup([
-		caseGroupA,
-		caseGroupB,
-		caseC1
-	]);
-
-	static function main() {
-		TesterSettings.hidePassedResults = true; // Just as the name says
-
-		test(allCases); // This is where the test actually starts
-	}
+    test(allCases); // This is where the test actually starts
+  }
 }
